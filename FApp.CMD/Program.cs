e478d143d517b9d11638edb5b1bc012921a5c7b6
EpsilonYega <amd7tashit@gmail.com>
2022-12-12 13:22:59 +0300

    Создание модели пользователя

---
+++ FApp.CMD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FApp.CMD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
